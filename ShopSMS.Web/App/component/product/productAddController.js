1febe7f66d36a20dff99658d57efe36415e06246
thuyenuit <nhatthu100@gmail.com>
2017-11-05 00:22:59 +0700

    Update 04/11/2017

--- ShopSMS.Web/App/component/product/productAddController.js
+++ ShopSMS.Web/App/component/product/productAddController.js
@@ -9,17 +9,46 @@
     function productAddController($scope, apiService, $interval,
         $filter, $ngBootbox, $state, notificationService) {
 
-        $scope.moreImages = [];
+        $scope.avatar = '/UploadFiles/images/no-image.jpg';
+        $scope.productInfo = {};
 
+        $scope.moreImages = [];
         $scope.ChooseImage = function () {
             var finder = new CKFinder();
             finder.selectActionFunction = function (fileUrl) {
-                //console.log(fileUrl);
                 $scope.$apply(function () {
-                    $scope.moreImages.push(fileUrl);
+
+                    var index = $scope.moreImages.indexOf(fileUrl);
+                    if (index > -1) {
+                        notificationService.displayWarning('Ảnh đã được lựa chọn trước đó');
+                    }
+                    else
+                    {
+                        $scope.moreImages.push(fileUrl);
+                        $scope.avatar = $scope.moreImages[0];
+                    } 
                 })               
             }
             finder.popup();       
+        }
+
+        $scope.ckeditorOptions = {
+            language: 'vi',
+            height: '200px'
+        }
+
+        $scope.fnRemoveImg = function (item) {
+            var index = $scope.moreImages.indexOf(item);
+            if (index > -1) {
+                $scope.moreImages.splice(index, 1);                
+            }
+
+            if ($scope.moreImages.length > 0) {
+                $scope.avatar = $scope.moreImages[0];
+            }
+            else {
+                $scope.avatar = '/UploadFiles/images/no-image.jpg';
+            }
         }
     }
 })(angular.module('sms.product'));
