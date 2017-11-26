678b547c3fa536e6110682789596d8103a3f6d22
thuyenuit <nhatthu100@gmail.com>
2017-11-27 00:07:35 +0700

    Cập nhật RouteConfig

--- ShopSMS.Web/App_Start/RouteConfig.cs
+++ ShopSMS.Web/App_Start/RouteConfig.cs
@@ -12,11 +12,48 @@ namespace ShopSMS.Web
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{*botdetect}", new { botdetect = @"(.*)BotDetectCaptcha\.ashx" });
+
+            //routes.MapRoute(
+            //   name: "Category",
+            //   url: "dtdd-{id}",
+            //   defaults: new { controller = "Category", action = "Index", id = UrlParameter.Optional },
+            //   namespaces: new string[] { "ShopSMS.Web.Controllers" }
+            //);
+
+            routes.MapRoute(
+               name: "Dien thoai",
+               url: "dien-thoai",
+               defaults: new { controller = "Category", action = "SmartPhoneView", id = UrlParameter.Optional },
+               namespaces: new string[] { "ShopSMS.Web.Controllers" }
+            );
+
+            routes.MapRoute(
+               name: "May tinh bang",
+               url: "may-tinh-bang",
+               defaults: new { controller = "Category", action = "TabletView", id = UrlParameter.Optional },
+               namespaces: new string[] { "ShopSMS.Web.Controllers" }
+            );
+
+            routes.MapRoute(
+               name: "Laptop",
+               url: "laptop",
+               defaults: new { controller = "Category", action = "LaptopView", id = UrlParameter.Optional },
+               namespaces: new string[] { "ShopSMS.Web.Controllers" }
+            );
+
+            routes.MapRoute(
+               name: "Phu kien",
+               url: "phu-kien",
+               defaults: new { controller = "Category", action = "AccessoriesView", id = UrlParameter.Optional },
+               namespaces: new string[] { "ShopSMS.Web.Controllers" }
+            );
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "ShopSMS.Web.Controllers" }
             );
         }
     }
