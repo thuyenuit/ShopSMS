e67f4d18de9ae6929baed1d74109154eec64cd08
thuyenuit <nhatthu100@gmail.com>
2017-11-27 00:11:02 +0700

    Comit VM

---
+++ ShopSMS.Web/ViewModelClient/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using ShopSMS.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopSMS.Web.ViewModelClient
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<ProductViewModel> ListProduct { get; set; }
+    }
+}
